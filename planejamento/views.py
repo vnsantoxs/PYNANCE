7b66b8ce76e3f21aaccbe9a54dd5befbabef596f
Vinicius Melchior <105757287+vnsantoxs@users.noreply.github.com>
2023-07-09 16:28:53 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .idea/.gitignore
    	new file:   .idea/Finance App(Django).iml
    	new file:   .idea/inspectionProfiles/profiles_settings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   core/__init__.py
    	new file:   core/__pycache__/__init__.cpython-311.pyc
    	new file:   core/__pycache__/settings.cpython-311.pyc
    	new file:   core/__pycache__/urls.cpython-311.pyc
    	new file:   core/__pycache__/wsgi.cpython-311.pyc
    	new file:   core/asgi.py
    	new file:   core/settings.py
    	new file:   core/urls.py
    	new file:   core/wsgi.py
    	new file:   db.sqlite3
    	new file:   extrato/__init__.py
    	new file:   extrato/__pycache__/__init__.cpython-311.pyc
    	new file:   extrato/__pycache__/admin.cpython-311.pyc
    	new file:   extrato/__pycache__/apps.cpython-311.pyc
    	new file:   extrato/__pycache__/models.cpython-311.pyc
    	new file:   extrato/__pycache__/urls.cpython-311.pyc
    	new file:   extrato/__pycache__/views.cpython-311.pyc
    	new file:   extrato/admin.py
    	new file:   extrato/apps.py
    	new file:   extrato/migrations/0001_initial.py
    	new file:   extrato/migrations/__init__.py
    	new file:   extrato/migrations/__pycache__/0001_initial.cpython-311.pyc
    	new file:   extrato/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   extrato/models.py
    	new file:   extrato/templates/novo_valor.html
    	new file:   extrato/templates/view_extrato.html
    	new file:   extrato/tests.py
    	new file:   extrato/urls.py
    	new file:   extrato/views.py
    	new file:   manage.py
    	new file:   media/icones/709107.png
    	new file:   media/icones/709107_1cwvWM1.png
    	new file:   media/icones/709107_H1sdJGy.png
    	new file:   perfil/__init__.py
    	new file:   perfil/__pycache__/__init__.cpython-311.pyc
    	new file:   perfil/__pycache__/admin.cpython-311.pyc
    	new file:   perfil/__pycache__/apps.cpython-311.pyc
    	new file:   perfil/__pycache__/models.cpython-311.pyc
    	new file:   perfil/__pycache__/urls.cpython-311.pyc
    	new file:   perfil/__pycache__/utils.cpython-311.pyc
    	new file:   perfil/__pycache__/views.cpython-311.pyc
    	new file:   perfil/admin.py
    	new file:   perfil/apps.py
    	new file:   perfil/migrations/0001_initial.py
    	new file:   perfil/migrations/__init__.py
    	new file:   perfil/migrations/__pycache__/0001_initial.cpython-311.pyc
    	new file:   perfil/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   perfil/models.py
    	new file:   perfil/templates/gerenciar.html
    	new file:   perfil/templates/home.html
    	new file:   perfil/tests.py
    	new file:   perfil/urls.py
    	new file:   perfil/utils.py
    	new file:   perfil/views.py
    	new file:   planejamento/__init__.py
    	new file:   planejamento/__pycache__/__init__.cpython-311.pyc
    	new file:   planejamento/__pycache__/admin.cpython-311.pyc
    	new file:   planejamento/__pycache__/apps.cpython-311.pyc
    	new file:   planejamento/__pycache__/models.cpython-311.pyc
    	new file:   planejamento/__pycache__/urls.cpython-311.pyc
    	new file:   planejamento/__pycache__/views.cpython-311.pyc
    	new file:   planejamento/admin.py
    	new file:   planejamento/apps.py
    	new file:   planejamento/migrations/__init__.py
    	new file:   planejamento/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   planejamento/models.py
    	new file:   planejamento/templates/definirplanejamento.html
    	new file:   planejamento/tests.py
    	new file:   planejamento/urls.py
    	new file:   planejamento/views.py
    	new file:   templates/bases/base.html
    	new file:   templates/static/extrato/view_extrato
    	new file:   templates/static/extrato/view_extrato.css
    	new file:   templates/static/geral/css/base.css
    	new file:   templates/static/perfil/css/home.css
    	new file:   templates/static/perfil/img/check.png
    	new file:   templates/static/perfil/img/entradas.png
    	new file:   templates/static/perfil/img/exit.png
    	new file:   templates/static/perfil/img/saidas.png

---
+++ planejamento/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from perfil.models import Categoria
+def definir_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'definir_planejamento.html', {'categorias': categorias})
